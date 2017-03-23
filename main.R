1762f4255245f3d8730937cffbce39d4c0ca7971
dcgretarex <dcgreatrex@aol.com>
2017-03-23 10:31:17 +0000

    update etf ticker symbols

--- main.R
+++ main.R
@@ -2,7 +2,7 @@
 # Title: main.reversals
 # Description: Compute reversal points for detecting channel boundaries and entry/exit points
 # Author: David Greatrex
-# Date: 30/11/2016                                                                          
+# Date: 23/03/2017                                                                          
 # Modifications:
 #-----------------------------------------------------------------------------------------------------
 
@@ -11,8 +11,8 @@
 #-------------------------------------
 wd = "/Users/dcg/_gitProjects/Public/finance_reversals"
 data_provider <- "yahoo" 
-start_month <- "2015-01"
-row_lookback <- 3
+start_month <- "2016-01"
+row_lookback <- 5
 
 #-------------------------------------
 # load functions
@@ -25,6 +25,13 @@ source(paste(wd,'functions/reversals.R',sep="/")) # load function script
 markets <- c('ftse_100','ftse_250')
 market <- read.table(paste(wd,'data',paste0(markets[1],'.txt'),sep="/"),sep="\t", header=FALSE)
 tickers <- market[,1]
+
+# etf ticker selection
+# ftse_long <- c('ISF.L','FTAL.L','XFEW.L','VMID.L','VUKE.L', 'IUKD.L')
+# ftse_short <- c('SUK2.L')
+# us_long <- c('CSPX.L', 'VUSA.L', 'R2SC.L', 'EQQQ.L')
+# commodities <- c('3OIL.L','SGLD.L','SSLV.L','PHPT.L','PHPD.L','CRUD.L','IH2O.L','GDX.L','SPGP.L')
+# tickers <- c(ftse_long, ftse_short, us_long, commodities)
 
 #-------------------------------------
 # get reversals
@@ -39,8 +46,8 @@ for (i in 1:length(tickers)){
   }
 
   # compute reversals
-  high <- reversals(data = dat$d[,2], lookback = row_lookback, ticker = s, is_High = 1)
-  low <- reversals(data = dat$d[,3], lookback = row_lookback, ticker = s, is_High = 0)
+  high <- reversals(data = dat$d[,4], lookback = row_lookback, ticker = s, is_High = 1)
+  low <- reversals(data = dat$d[,4], lookback = row_lookback, ticker = s, is_High = 0)
   
   # plot reversals
   chartSeries(dat$d, name = s, type = "candlesticks", 
